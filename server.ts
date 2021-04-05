f7d5bc0ae6b8b8cd42453bf3fd5af7cbe7c2fd50
daniel-clain <daniel.clain@hotmail.com>
2021-04-05 16:27:29 +1000

    test

--- server.ts
+++ server.ts
@@ -4,7 +4,12 @@ import {GameHost} from './source-code/game-host/game-host';
 env.config()
 
 console.log('come on mate...')
-new GameHost()
+let gameHost: GameHost
+try{
+  gameHost = new GameHost()
+}catch(e){
+  console.log('error :>> ', e);
+}
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +18,9 @@ app.get('/favicon.ico', (req, res) => {
   res.sendFile(`${__dirname}/favicon.ico`)
 })
 app.get('*', (req, res) => {
-  console.log('req.url :>> ', req.url);
-  res.send('Hello World!')
+  const msg = `connected websocket clients ${gameHost.state.connectedClients.length}`
+  console.log('url: ', req.url);
+  console.log('msg :>> ', msg);
+  res.send(msg)
 })
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- source-code/game-components/abilities-reformed/abilities/try-to-win-game.ts
+++ source-code/game-components/abilities-reformed/abilities/try-to-win-game.ts
@@ -9,74 +9,16 @@ const tryToWinGame: Ability = {
   name: 'Try To Win',
   cost: { money: 10, actionPoints: 1 },
   possibleSources: ['Manager'],
-  possibleTargets: ['fighter not owned by manager'],
+  possibleTargets: [],
   executes: 'End Of Manager Options Stage',
   canOnlyTargetSameTargetOnce: false
 }
 
 export const tryToWinGameServer: ServerAbility = {
   execute(abilityData: AbilityData, game: Game){
-    const fighter = game.has.fighters.find(fighter => fighter.name == abilityData.target.name)
     
-    let assaulter: Employee
-    let assaultersManager: Manager
-    for(let manager of game.has.managers){
-      assaulter = manager.has.employees.find(employee => employee.name == abilityData.source.name)
-      if(assaulter){
-        assaultersManager = manager
-        break
-      }
-    }
+    const sourceManager = game.has.managers.find(m => m.has.name == abilityData.source.name)
 
-    
-    if(fighter.state.dead){
-      assaultersManager.functions.addToLog({message: `Attempt to assault ${abilityData.target.name} failed beacuse he was already found dead`, type:'employee outcome'})
-      return
-    }
-
-    let success
-    let guardBlocked
-    const guardLevel = fighter.state.guards.reduce((totalSkill, thugGuardingFighter) => totalSkill += thugGuardingFighter.skillLevel, 0)
-    if(guardLevel > 0){
-      const randomNum = random(100, true)
-      if(randomNum < 15 - guardLevel * 5 + assaulter.skillLevel * 5)
-        success = true
-      else
-        guardBlocked = true
-    }
-    else {
-      const randomNum = random(100, true)
-      if(randomNum < 85 + assaulter.skillLevel * 5)
-        success = true
-
-    }
-    if(success){
-      fighter.state.injured = true
-      game.has.roundController.preFightNewsStage.newsItems.push({
-        newsType: 'fighter was assaulted',
-        headline: `${fighter.name} Assaulted!`,
-        message: `${fighter.name} has been assaulted, his injuries will affect his fight performance`
-      })
-      assaultersManager.functions.addToLog({
-        message: `${assaulter.name} has successfully assaulted ${fighter.name} and he is now injured`
-      })
-    }
-    else if(guardBlocked){
-      game.has.roundController.preFightNewsStage.newsItems.push({
-        newsType: 'guards protect fighter from being assaulted',
-        headline: `${fighter.name} Guarded from Assailant!`,
-        message: `Guards have protected ${fighter.name} from an attempted assault`
-      })     
-      assaultersManager.functions.addToLog({
-        type: 'employee outcome',
-        message: `${assaulter.profession} ${assaulter.name} failed to assault target fighter ${abilityData.target.name} because he was guarded by thugs`
-      })
-    }
-    else
-    assaultersManager.functions.addToLog({
-        type: 'employee outcome', 
-        message: `${assaulter.profession} ${assaulter.name} failed to assault target fighter ${abilityData.target.name}`
-      })
       
 
 
