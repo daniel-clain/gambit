821cd956b63c6fbf78e7a1a89238a09a1386dfac
daniel-clain <daniel.clain@hotmail.com>
2024-12-11 03:33:43 +1100

    change

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "gambit", // Name of your app
+      script: "npm",
+      args: "run start-server", // The npm script to run
+      interpreter: "none", // Use the system shell to run the npm script
+      env: {
+        NODE_ENV: "development", // Environment variables for development
+      },
+      env_production: {
+        NODE_ENV: "production", // Environment variables for production
+      },
+      watch: true, // Optional: Restart app when files change
+    },
+  ],
+}
