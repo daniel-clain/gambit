b3e99b9d0731f105fb10956e0f4ad977e8670a9b
daniel-clain <daniel.clain@hotmail.com>
2024-12-14 18:25:51 +1100

    x

--- source-code/client/different-build-modes/main-game/main-game.tsx
+++ source-code/client/different-build-modes/main-game/main-game.tsx
@@ -1,32 +1,34 @@
-
-import * as React from 'react';
-import '../../styles/global.scss';
-import {Game_View} from '../../views/game/game.view'
-import { initialSetup } from '../../front-end-service/front-end-service';
-import { Login_View } from '../../views/pre-game/login.view';
-import { Lobby_View } from '../../views/pre-game/lobby.view';
-import { observer } from 'mobx-react';
-import { frontEndState } from '../../front-end-state/front-end-state';
+import { observer } from "mobx-react"
+import { useEffect } from "react"
+import { initialSetup } from "../../front-end-service/front-end-service"
+import { frontEndState } from "../../front-end-state/front-end-state"
+import "../../styles/global.scss"
+import { Game_View } from "../../views/game/game.view"
+import { Lobby_View } from "../../views/pre-game/lobby.view"
+import { Login_View } from "../../views/pre-game/login.view"
 
 initialSetup()
 
 export const MainGame_C = observer(() => {
-
+  useEffect(() => {
+    // This will scroll the page down slightly to hide the address bar
+    if (window.scrollY === 0) {
+      window.scrollTo(0, 1)
+    }
+  }, [])
   const {
-    clientUIState: { 
+    clientUIState: {
       isConnectedToGameHost,
       isConnectedToWebsocketServer,
-      clientPreGameUIState: {hasGameData}
-    }
+      clientPreGameUIState: { hasGameData },
+    },
   } = frontEndState
 
-
-
-
-  return hasGameData ? 
-  <Game_View />
-  : isConnectedToWebsocketServer && isConnectedToGameHost ? 
+  return hasGameData ? (
+    <Game_View />
+  ) : isConnectedToWebsocketServer && isConnectedToGameHost ? (
     <Lobby_View />
-    : <Login_View /> 
-       
+  ) : (
+    <Login_View />
+  )
 })
